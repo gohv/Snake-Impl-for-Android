e7f928614d174db28c99fc1f1b5b85762caeca31
gohv <hristovosotirovgeorgi@gmail.com>
2017-02-02 11:59:24 +0200

    directions fixed

---
+++ app/src/main/java/georgyhristov/xyz/mrnom/framework/math/Vector2.java
@@ -0,0 +1,15 @@
+package georgyhristov.xyz.mrnom.framework.math;
+
+/**
+ * Created by gohv on 31.01.17.
+ */
+public class Vector2 {
+
+    public static float TO_RADIANS = (1 / 180.0f) * (float) Math.PI;
+    public static float TO_DEGREES = (1 / (float) Math.PI) * 180;
+
+    public Vector2() {
+    }
+
+
+}
